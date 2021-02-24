d05cae80d8abc58e52a801f00e968b16a5f0293a
tarotata <milkyakan@yahoo.co.jp>
2021-02-24 14:26:48 +0000

    Edit plural Models

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,3 +1,5 @@
 class Goal < ApplicationRecord
   belongs_to :user
+  has_many :todos, -> { order("done ASC, position ASC").includes(:goal) }, dependent: :destroy
+  validates :title, presence: true
 end
